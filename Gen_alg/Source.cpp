397d6a6a0fe41887a90e8afc76f1da8c2f923e4e
Vladimir Novoselov <catran258@gmail.com>
2024-08-16 23:01:12 +0300

    deleted excess

--- Gen_alg/Source.cpp
+++ Gen_alg/Source.cpp
@@ -1,21 +1,9 @@
 #include "Engine_bool.h"
 #include "Engine_double.h"
 #include "Engine_int.h"
-#include <chrono>
-
-//при 10 парам, 50 популяции и 500 поколений без инлайн
-//2.9
-//2.745 с inline
-//0.8 с inline release
-//0.8 без инлайн релиз
 
 void startGenetic()
 {
-	using std::chrono::steady_clock;
-	using std::chrono::duration_cast;
-	using std::chrono::milliseconds;
-	auto start1{ steady_clock::now() }; // Получаем текущее время
-
 	engine_DOUBLE geneticEngine;
 
 	geneticEngine.setCountParams(10);
@@ -34,27 +22,9 @@ void startGenetic()
 		geneticEngine.calculationFitnessAll();
 		geneticEngine.championChek();
 	}
-
-	auto end1{ steady_clock::now() }; // Получаем текущее время
-
-	// Вычисляем и отображаем длительность сортировки в миллисекундах
-	auto time1{ duration_cast<milliseconds>(end1 - start1) };
-	std::cout << "Time: " << (time1.count() / 1000.0) << " seconds\n";
-}
-
-void test()
-{
-	cout << "Int " << sizeof(int) << endl;
-	cout << "Double " << sizeof(double) << endl;
-	cout << "Char " << sizeof(char) << endl;
-	cout << "String " << sizeof(string) << endl;
-	cout << "Long int " << sizeof(long int) << endl;
-	cout << "Long long int " << sizeof(long long int) << endl;
-	cout << "Long double " << sizeof(long double) << endl;
 }
 
 int main()
 {
-	//test();
 	startGenetic();
 }
